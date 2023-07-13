c12136f1d6e694e1237cd23a61d07f3564fe7902
fahad <bsse1105@iit.du.ac.bd>
2023-07-14 03:50:48 +0600

    texttospeech

---
+++ Frontend/src/comp/MarketPlace.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import NavBar from "./navBar";
+
+function Marketplace() {
+  const [books, setBooks] = useState([
+    {
+      id: 1,
+      title: "Book 1",
+      author: "Author 1",
+      coverImage: "/assistant.png",
+      description: "Description of Book 1",
+      previewUrl: "https://example.com/book1-preview.pdf",
+    },
+    {
+      id: 2,
+      title: "Book 2",
+      author: "Author 2",
+      coverImage: "/assistant.png",
+      description: "Description of Book 2",
+      previewUrl: "https://example.com/book2-preview.pdf",
+    },
+    // Add more books here
+  ]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedBook, setSelectedBook] = useState(null);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleBookClick = (book) => {
+    setSelectedBook(book);
+  };
+
+  const handleDownload = (book) => {
+    // Implement the logic to download the book
+    console.log("Downloading book:", book.title);
+  };
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <>
+      <NavBar />
+      <div className="flex justify-center">
+        <div className="w-[80%]">
+          <div className="container px-4 py-10 mx-auto overflow-x-hidden">
+            <h1 className="text-3xl font-bold mb-4 pt-10 text-center">Book Marketplace</h1>
+            <div className="w-1/4 mx-auto h-1 bg-gradient-to-r from-[#5046E5] to-transparent"></div>
+
+
+            {/* Search bar */}
+            <div className="py-5">
+            <input
+              type="text"
+              placeholder="Search books..."
+              className="w-full mb-4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+            </div>
+
+            {/* Book list */}
+            <div className="grid grid-cols-3 gap-4">
+              {filteredBooks.map((book) => (
+                <div
+                  key={book.id}
+                  className="border border-gray-300 rounded-md p-4 cursor-pointer hover:bg-gray-100"
+                  onClick={() => handleBookClick(book)}
+                >
+                  <img
+                    src={book.coverImage}
+                    alt={book.title}
+                    className="w-full h-48 object-contain mb-4"
+                  />
+                  <h3 className="text-lg font-bold">{book.title}</h3>
+                  <p className="text-gray-600">{book.author}</p>
+                </div>
+              ))}
+            </div>
+
+            {/* Book details */}
+            {selectedBook && (
+              <div className="mt-8">
+                <h2 className="text-xl font-bold">{selectedBook.title}</h2>
+                <p className="text-gray-600">{selectedBook.author}</p>
+                <p>{selectedBook.description}</p>
+
+                {/* Book preview */}
+                <iframe
+                  src={selectedBook.previewUrl}
+                  className="w-full h-96 mt-4"
+                  title="Book Preview"
+                ></iframe>
+
+                {/* Download button */}
+                <button
+                  className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 mt-4 rounded"
+                  onClick={() => handleDownload(selectedBook)}
+                >
+                  Download
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Marketplace;
