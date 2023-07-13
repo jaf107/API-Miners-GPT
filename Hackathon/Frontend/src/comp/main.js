b6884de1c55883873925b225a8ddc7f8b2d04b2d
fahad <bsse1105@iit.du.ac.bd>
2023-07-13 17:34:42 +0600

    texttospeech

--- Hackathon/Frontend/src/comp/main.js
+++ Hackathon/Frontend/src/comp/main.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { TextField, Button, Modal } from "@material-ui/core";
-import { Send, Mic } from "@material-ui/icons";
+import { Send, Mic, } from "@material-ui/icons";
 import Chat from "./chat";
+import {Image} from "react-feather"
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
+// ...
 function Main() {
   const [message, setMessage] = useState("");
   const [chat, setChat] = useState([
@@ -15,6 +18,7 @@ function Main() {
   const [isListening, setIsListening] = useState(false);
   const [recordingDuration, setRecordingDuration] = useState(0);
   const [showModal, setShowModal] = useState(false);
+  const [file, setFile] = useState(null);
 
   useEffect(() => {
     let intervalId = null;
@@ -73,6 +77,39 @@ function Main() {
     }
   };
 
+  const handleFileUpload = async (event) => {
+    const uploadedFile = event.target.files[0];
+    
+
+    if (uploadedFile) {
+      if (uploadedFile.type === "image/jpeg" || uploadedFile.type === "image/png") {
+        const imageData = new FormData();
+        console.log(imageData);
+        imageData.append("file", uploadedFile);
+
+        try {
+          const response = await axios.post("http://localhost:4000/ocr", imageData);
+          const extractedText = response.data.text;
+
+          setMessage((prevMessage) => prevMessage + extractedText);
+        } catch (error) {
+          console.error("An error occurred during OCR:", error);
+        }
+      } else if (uploadedFile.type === "text/plain") {
+        const fileReader = new FileReader();
+
+        fileReader.onload = (e) => {
+          const textContent = e.target.result;
+          setMessage((prevMessage) => prevMessage + textContent);
+        };
+
+        fileReader.readAsText(uploadedFile);
+      } else {
+        console.log("Unsupported file format");
+      }
+    }
+  };
+
   const handleSubmit = async () => {
     if (message !== "") {
       setChat((prevChat) => [
@@ -123,38 +160,60 @@ function Main() {
             return <Chat role={data.role} content={data.content} />;
           })}
       </div>
-      <div className="flex w-[70%] my-[2rem]">
-        <TextField
-          className="mr-2"
-          label="Prompt"
-          variant="outlined"
-          onChange={(e) => handleChange(e.target.value)}
-          value={message}
-          fullWidth
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          endIcon={<Send />}
-          onClick={handleSubmit}
-        />
-        <Button
-          variant="contained"
-          color={isListening ? "secondary" : "primary"}
-          onClick={handleVoiceInput}
-        >
-          <Mic />
-        </Button>
-      </div>
+     
+
+<div className="flex w-[70%] my-[2rem]">
+  <TextField
+    className="mr-2"
+    label="Prompt"
+    variant="outlined"
+    onChange={(e) => handleChange(e.target.value)}
+    value={message}
+    fullWidth
+  />
+  <Button
+    variant="contained"
+    color="primary"
+    endIcon={<Send />}
+    onClick={handleSubmit}
+  />
+  <Button
+    variant="contained"
+    color={isListening ? "secondary" : "primary"}
+    onClick={handleVoiceInput}
+  >
+    <Mic />
+  </Button>
+
+
+<label htmlFor="upload-input">
+  <Button
+    variant="contained"
+    component="span"
+    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
+  >
+    <AttachFileIcon className="mr-2" />
+    Upload File
+  </Button>
+</label>
+<input
+  id="upload-input"
+  type="file"
+  accept="image/jpeg, image/png, text/plain"
+  className="hidden"
+  onChange={handleFileUpload}
+/>
+
+</div>
+
       <Modal open={showModal} onClose={handleCloseModal}>
-      <div className="w-[300px] h-[200px] bg-white text-black flex flex-col items-center justify-center absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <p>{`Recording duration: ${recordingDuration}s`}</p>
-        <Button variant="contained" color="primary" onClick={handleCloseModal}>
-          Close
-        </Button>
-      </div>
-    </Modal>
-    
+        <div className="w-[300px] h-[200px] bg-white text-black flex flex-col items-center justify-center absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <p>{`Recording duration: ${recordingDuration}s`}</p>
+          <Button variant="contained" color="primary" onClick={handleCloseModal}>
+            Close
+          </Button>
+        </div>
+      </Modal>
     </div>
   );
 }
