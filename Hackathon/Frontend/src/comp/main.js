65a1750a1660df4b43ff2612672a4515b524657f
fahad <bsse1105@iit.du.ac.bd>
2023-07-13 19:33:21 +0600

    texttospeech

--- Hackathon/Frontend/src/comp/main.js
+++ Hackathon/Frontend/src/comp/main.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { TextField, Button, Modal } from "@material-ui/core";
-import { Send, Mic, } from "@material-ui/icons";
+import { Send, Mic } from "@material-ui/icons";
 import Chat from "./chat";
-import {Image} from "react-feather"
+import { Image } from "react-feather";
 import AttachFileIcon from "@material-ui/icons/AttachFile";
 
 // ...
@@ -79,16 +79,22 @@ function Main() {
 
   const handleFileUpload = async (event) => {
     const uploadedFile = event.target.files[0];
-    
 
     if (uploadedFile) {
-      if (uploadedFile.type === "image/jpeg" || uploadedFile.type === "image/png") {
+      if (
+        uploadedFile.type === "image/jpeg" ||
+        uploadedFile.type === "image/png"
+      ) {
         const imageData = new FormData();
-        console.log(imageData);
+      
         imageData.append("file", uploadedFile);
+        console.log(imageData.get("file"));
 
         try {
-          const response = await axios.post("http://localhost:4000/ocr", imageData);
+          const response = await axios.post(
+            "http://localhost:4000/ocr",
+            imageData
+          );
           const extractedText = response.data.text;
 
           setMessage((prevMessage) => prevMessage + extractedText);
@@ -160,11 +166,10 @@ function Main() {
             return <Chat role={data.role} content={data.content} />;
           })}
       </div>
-     
 
-<div className="flex w-[70%] my-[2rem]">
+      <div className="flex w-[70%] my-[2rem]">
   <TextField
-    className="mr-2"
+    className="mr-2 text-sm"
     label="Prompt"
     variant="outlined"
     onChange={(e) => handleChange(e.target.value)}
@@ -176,42 +181,44 @@ function Main() {
     color="primary"
     endIcon={<Send />}
     onClick={handleSubmit}
+    size="small"
   />
   <Button
     variant="contained"
     color={isListening ? "secondary" : "primary"}
     onClick={handleVoiceInput}
+    size="small"
   >
     <Mic />
   </Button>
-
-
-<label htmlFor="upload-input">
-  <Button
-    variant="contained"
-    component="span"
-    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center"
-  >
-    <AttachFileIcon className="mr-2" />
-    <span className="hidden md:block ">
-    Upload File
-    </span>
-  </Button>
-</label>
-<input
-  id="upload-input"
-  type="file"
-  accept="image/jpeg, image/png, text/plain"
-  className="hidden"
-  onChange={handleFileUpload}
-/>
-
+  <label htmlFor="upload-input">
+    <Button
+      variant="contained"
+      component="span"
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded flex items-center text-sm"
+      size="small"
+    >
+      <AttachFileIcon className="mr-2" />
+      <span className="hidden md:block">Upload File</span>
+    </Button>
+  </label>
+  <input
+    id="upload-input"
+    type="file"
+    accept="image/jpeg, image/png, text/plain"
+    className="hidden"
+    onChange={handleFileUpload}
+  />
 </div>
 
       <Modal open={showModal} onClose={handleCloseModal}>
         <div className="w-[300px] h-[200px] bg-white text-black flex flex-col items-center justify-center absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2">
           <p>{`Recording duration: ${recordingDuration}s`}</p>
-          <Button variant="contained" color="primary" onClick={handleCloseModal}>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleCloseModal}
+          >
             Close
           </Button>
         </div>
