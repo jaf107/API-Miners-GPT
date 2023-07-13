c3f73bd72c3546c3fd0dbd1246dba0c00ae249d5
Abu Jafar Saifullah Mahin <bsse1109@iit.du.ac.bd>
2023-07-13 23:36:43 +0600

    refactored

--- Backend/Controllers/ChatbotController.cs
+++ Backend/Controllers/ChatbotController.cs
@@ -31,34 +31,7 @@ namespace Chatbot.Controllers
                 return Ok(responseMsg);
             else
                 return BadRequest("Not found");
-            //DotNetEnv.Env.Load();
-            //string apikey = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
-            //string answer=string.Empty;
-
-            //var openai = new OpenAIAPI(apikey);
-            //CompletionRequest completion = new CompletionRequest();
-            //completion.Prompt = request.Message;
-            //completion.Model = OpenAI_API.Models.Model.DavinciText;
-            //completion.MaxTokens = 100;
-
-            //var result=openai.Completions.CreateCompletionsAsync(completion);
-
-            //if (result != null)
-            //{
-            //    foreach (var item in result.Result.Completions)
-            //    {
-            //        answer = item.Text;
-            //    }
-
-            //    var responseMsg = new AnswerResponse()
-            //    {
-            //        ResponseMessage = answer.Trim()
-            //    };
-
-            //    return Ok(responseMsg);
-            //}
-            //else
-            //    return BadRequest("Not found");
+            
 
         }
 
