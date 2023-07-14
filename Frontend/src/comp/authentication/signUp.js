0b76974e367688ab10f5f64102e37595e2d7aa0e
fahad <bsse1105@iit.du.ac.bd>
2023-07-14 08:32:18 +0600

    texttospeech

--- Frontend/src/comp/authentication/signUp.js
+++ Frontend/src/comp/authentication/signUp.js
@@ -48,19 +48,55 @@ const SignUp = () => {
             validationSchema={validationSchema}
             onSubmit={handleSubmit}
           >
-            <Form className="space-y-6">
-              {/* Form fields */}
-              {/* ... */}
-              
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Sign up
-                </button>
+          <Form className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900 flex">
+              Email address
+            </label>
+            <div className="mt-2">
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                autoComplete="email"
+                className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <ErrorMessage name="email" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Password
+              </label>
+              <div className="text-sm">
+                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                  Forgot password?
+                </a>
               </div>
-            </Form>
+            </div>
+            <div className="mt-2">
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                autoComplete="current-password"
+                className="block px-2 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+              <ErrorMessage name="password" component="div" className="text-red-500 text-sm mt-1" />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign up
+            </button>
+          </div>
+        </Form>
           </Formik>
 
           <p className="mt-10 text-center text-sm text-gray-500">
